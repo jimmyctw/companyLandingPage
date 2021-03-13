571f52d08b9dc16cb439f419a640ec5adac1628d
jimmyctw <66037440+jimmyctw@users.noreply.github.com>
2021-03-14 00:42:23 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const serviceBtns = document.getElementsByClassName('serviceBtn');
-const formAndSend = document.getElementsById('formAndSend')
+const formBox = document.getElementById('formBox')
 
 
 
@@ -19,24 +19,27 @@ submitBtn.addEventListener('click', (event)=>{
     const fmail = document.getElementById('fmail').value;
     const fmobile = document.getElementById('fmobile').value;
     const fmessage = document.getElementById('fmessage').value;
-    const submitBtn = document.getElementById('submitBtn').value;
-    form-box.classList.add('displayNone')
-    confirmContainerBuild()
+    confirmContainerBuild(fname, fmail, fmobile, fmessage);
 })
 
 function confirmContainerBuild(fname, fmail, fmobile, fmessage){
-    const confirmContainer = document.createElement('div').classList.add('confirmContainer')
-    const confirmCotent = document.createElement('div').classList.add('confirmCotent')
+    const formAndSend = document.getElementById('formAndSend');
+    const confirmContainer = document.createElement('div')
+    confirmContainer.classList.add('confirmContainer');
+    const confirmCotent = document.createElement('div')
+    confirmCotent.classList.add('confirmCotent');
     const confirmCotentVal = `
-        <h1>${fname}</h1>
-        <span>${fmail}</span>
-        <span>${fmobile}</span>
-        <p>${fmessage}</p>
+        <h1> ${fname}</h1>
+        <span> Email: ${fmail}</span>
+        <span> Mobile: ${fmobile}</span>
+        <p> Message: ${fmessage}</p>
     `
-    confirmCotent.innerHTML = confirmCotentVal
-    const sendContentBtn = document.createElement('button').classList.add(sendContentBtn)
+    confirmCotent.innerHTML = confirmCotentVal;
+    const sendContentBtn = document.createElement('button');
+    sendContentBtn.classList.add('sendContentBtn')
     sendContentBtn.innerHTML = "send";
     confirmContainer.appendChild(confirmCotent).appendChild(sendContentBtn)
     formAndSend.appendChild(confirmContainer)
+    formBox.classList.add('displayNone')
 
 }
